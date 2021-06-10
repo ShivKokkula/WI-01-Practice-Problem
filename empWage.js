d7a00014e0d9e037caa3a19ed04f005d8f1093bd
ShivKokkula <shivbk987@gmail.com>
2021-06-10 19:43:31 +0530

    Refacor - added UC-4 feature

--- empWage.js
+++ empWage.js
@@ -5,9 +5,9 @@ const IS_FULL_TIME = 2;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 2;
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -19,6 +19,10 @@ function getWorkingHours(empCheck) {
             return 0;
     }  
 }
-empHrs = getWorkingHours(empCheck);
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
